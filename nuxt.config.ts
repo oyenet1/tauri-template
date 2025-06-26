279df9c6bb1cae822e9e4887846b7bda6b27ec40
oyenet1 <bowofadeoyerinde@gmail.com>
2025-06-26 01:29:40 +0100

    integrated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  ssr: false,
+  // Enables the development server to be discoverable by other devices when running on iOS physical devices
+  devServer: { host: process.env.TAURI_DEV_HOST || "localhost" },
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://v2.tauri.app/reference/environment-variables/
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+    },
+  },
 
   modules: [
     "@nuxt/image",
